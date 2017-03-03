d941ffc729db1fa32ca8c320137477d853a52784
avadarad <avadanii@tiscali.it>
2017-03-03 15:23:19 +0200

    made the portfolio section

--- app/js/scripts/App.js
+++ app/js/scripts/App.js
@@ -1,7 +1,9 @@
 import TestModule from './modules/TestModule';
 import StickFooter from './modules/StickFooter';
 import Carousel from './modules/Carousel';
+import Mixitup from './modules/Mixitup';
 
 var testModule = new TestModule();
 var stickFooter = new StickFooter();
 var carousel = new Carousel();
+var mixitup = new Mixitup();
---
+++ app/js/scripts/modules/Mixitup.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+// import slick from 'slick-carousel';
+import mixitup from 'mixitup';
+
+class Mixitup {
+  constructor() {
+    // cache elements from page
+    this.links = $('.portfolio__filters-item a');
+    this.mixer = $('.portfolio__list');
+    mixitup(this.mixer);
+    this.events();
+  }
+
+  events() {
+    this.links.on('click', function(event) {
+      event.preventDefault();
+      $('.btn--filters').removeClass('btn--active');
+      $(this).addClass('btn--active');
+    });
+  }
+
+  // custom functions
+  logSomething() {
+    console.log("car!");
+  }
+}
+
+export default Mixitup;
